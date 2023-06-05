d383ee710caebc3ebde731d28687a240c0647d3e
Utsav Singh <singhutsavofficial@gmail.com>
2023-06-05 18:48:27 +0530

    setup satoshi bold font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import Nav from './components/nav'
 import './App.css'
 function App() {
 
   return (
     <>
-      
+      <Nav />
     </>
   )
 }
---
+++ src/components/nav.jsx
@@ -0,0 +1,14 @@
+import './nav.css'
+const Nav = ()=>{
+    return(
+        <>
+        <div className="nav-container">
+            <div className="nav-logo">
+               <h4>utsav</h4>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Nav;
