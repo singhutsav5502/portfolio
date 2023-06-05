fb0d7096992c252184fe3bdf9c0481ff5855e362
Utsav Singh <singhutsavofficial@gmail.com>
2023-06-05 20:21:15 +0530

    finalise navbar animations

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -7,15 +7,19 @@ const Nav = () => {
                     <h4>utsav</h4>
                 </div>
                 <div className="nav-current-status">
-                    <h4>Constantly Learning</h4>
+                    <p>Constantly Learning</p>
                 </div>
                 <div className="nav-link-container">
-                    <a href="#">Projects,<span className="nav-link-line"></span></a>
+                    <a href="#"><p>Projects,</p>  <span className="nav-link-line"></span></a>
 
-                    <a href="#">About<span className="nav-link-line"></span></a>
+                    <a href="#"><p>About</p><span className="nav-link-line"></span></a>
                 </div>
                 <div className="nav-mail-container">
-                    <a href="#">hello@utsav.com</a>
+                    <a href="#"><p>hello@utsav.com</p>
+                        <span id="mail-line-1"></span>
+                        <span id="mail-line-2"></span>
+                    </a>
+
                 </div>
             </div>
         </>
