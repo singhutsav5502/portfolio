b44a2c01e4381e685fc155d190817a3cdf95c5fd
Utsav Singh <singhutsavofficial@gmail.com>
2023-06-06 22:46:00 +0530

    initial setup of hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Nav from './components/nav/nav'
+import Hero from './components/hero/hero'
 import './App.css'
 function App() {
 
   return (
     <>
       <Nav />
+      <Hero />
     </>
   )
 }
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,12 @@
+import './hero.css';
+
+const Hero = ()=>{
+    return(
+        <>
+        <div className="hero-container">
+
+        </div>
+        </>
+    )
+}
+export default Hero;
