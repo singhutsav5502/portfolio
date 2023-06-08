ea28052bd8768ed2162158fb593d8699a37d76f5
Utsav Singh <singhutsavofficial@gmail.com>
2023-06-08 15:18:37 +0530

    add skills section

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -5,21 +5,26 @@ const Skills = () => {
 
     return (
         <>
+
             <div className="skills-container" id="skills">
-                <div className="skills-grid-maker">
-                    <SkillCard title="HTML 5" logo="./src/assets/HTML5_Badge_512.png">
-                        HTML5 is a markup language used for structuring and presenting content on the World Wide Web.
-                    </SkillCard>
-                    <SkillCard title="HTML 5" logo="./src/assets/HTML5_Badge_512.png">
-                        HTML5 is a markup language used for structuring and presenting content on the World Wide Web.
-                    </SkillCard>
-                    <SkillCard title="HTML 5" logo="./src/assets/HTML5_Badge_512.png">
-                        HTML5 is a markup language used for structuring and presenting content on the World Wide Web.
-                    </SkillCard>
-                    <SkillCard title="HTML 5" logo="./src/assets/HTML5_Badge_512.png">
-                        HTML5 is a markup language used for structuring and presenting content on the World Wide Web.
-                    </SkillCard>
-                </div>
+                <div className="skills-container-title">SKILLS</div>
+                {/* <div className="skills-grid-maker"> */}
+                <SkillCard title="HTML 5" logo="./HTML5.png">
+                    {/* HTML5 is a markup language used for structuring and presenting content on the World Wide Web. */}
+                </SkillCard>
+                <SkillCard title="CSS 3" logo="./CSS3.png">
+                    {/* CSS is a style sheet language used for describing the presentation of a document written in a markup language. */}
+                </SkillCard>
+                <SkillCard title="Javascript" logo="./JS.png">
+                    {/* HTML5 is a markup language used for structuring and presenting content on the World Wide Web. */}
+                </SkillCard>
+                <SkillCard title="React JS" logo="./React.png">
+                    {/* HTML5 is a markup language used for structuring and presenting content on the World Wide Web. */}
+                </SkillCard>
+                <SkillCard title="C++" logo="./CPP.png">
+                    {/* HTML5 is a markup language used for structuring and presenting content on the World Wide Web. */}
+                </SkillCard>
+                {/* </div> */}
             </div>
         </>
     )
